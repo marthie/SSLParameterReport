48e96f86a8f4675dde4894e36bc6360baab74f6d
marius <marius.thiemann@ploin.de>
2018-05-14 17:59:32 +0200

    adjust babel env support

--- src/main/resources/react/webpack.config.js
+++ src/main/resources/react/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
                 use: {
                     loader: "babel-loader"
                 }
