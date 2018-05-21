25436f3da41aa817dbe0e6e4bd86a9c8c8fb0950
marius <marius.thiemann@ploin.de>
2018-05-21 12:49:51 +0200

    finish certificates

--- src/main/resources/react/src/ssl-report/components/ReportCertificates.js
+++ src/main/resources/react/src/ssl-report/components/ReportCertificates.js
@@ -5,10 +5,11 @@ export default function ReportCertifictaes({certificates}) {
     const certificateSections = [];
 
     certificates.map((certificateObject)=> {
-        console.log("rendeering " + certificateObject.certificatesChain +
+        console.log("rendeering " + certificateObject.certificatesChain.length +
             " certificates for version " + certificateObject.version);
 
-
+        certificateSections.push(<CertificateRows version={certificateObject.version}
+                                                  certificateChain={certificateObject.certificatesChain} />);
     });
 
     return (<table className="table table-bordered">
@@ -22,24 +23,78 @@ ReportCertifictaes.propTypes = {
     certificates: PropTypes.array.isRequired
 };
 
-function CertificateRow({version, certificateChain}) {
-    const certificateRows = [];
+function CertificateRows({version, certificateChain}) {
+    const certificateDetailRows = [];
 
     certificateChain.map((certificate)=>{
-
+        certificateDetailRows.push(<CertificateDetailRow version={version} certificate={certificate} />);
     });
 
-    return null;
+    return (<React.Fragment>{certificateDetailRows}</React.Fragment>);
 }
 
-CertificateRow.propTypes = {
+CertificateRows.propTypes = {
     version: PropTypes.string.isRequired,
     certificateChain: PropTypes.array.isRequired
 };
 
-function CertificateDetails({version, certificate}) {
-    const row = (<tr>
-        <td></td>
-    </tr>);
+function CertificateDetailRow({version, certificate}) {
+    const row = (<React.Fragment>
+        <tr>
+            <th scope="row" rowSpan="12">{version}</th>
+            <th scope="row">Send in order:</th>
+            <td>{certificate.order}</td>
+        </tr>
+        <tr>
+            <th scope="row">Certificate Version:</th>
+            <td>{certificate.version}</td>
+        </tr>
+        <tr>
+            <th scope="row">Subject:</th>
+            <td>{certificate.subjectName}</td>
+        </tr>
+        <tr>
+            <th scope="row">Issuer:</th>
+            <td>{certificate.issuerName}</td>
+        </tr>
+        <tr>
+            <th scope="row">Alternative Names:</th>
+            <td>{certificate.alternativeNames}</td>
+        </tr>
+        <tr>
+            <th scope="row">Not Before:</th>
+            <td>{certificate.notBefore}</td>
+        </tr>
+        <tr>
+            <th scope="row">Not After:</th>
+            <td>{certificate.notAfter}</td>
+        </tr>
+        <tr>
+            <th scope="row">Public Key Algorithm:</th>
+            <td>{certificate.pubKeyName}</td>
+        </tr>
+        <tr>
+            <th scope="row">Public Key Size:</th>
+            <td>{certificate.pubKeySize}</td>
+        </tr>
+        <tr>
+            <th scope="row">Signature Algorithm:</th>
+            <td>{certificate.signatureAlgorithm}</td>
+        </tr>
+        <tr>
+            <th scope="row">Fingerprint:</th>
+            <td>{certificate.fingerprint}</td>
+        </tr>
+        <tr>
+            <th scope="row">CRL Distribution Points:</th>
+            <td>{certificate.crlDistributionPoints}</td>
+        </tr>
+    </React.Fragment>);
+
+    return row;
 }
 
+CertificateDetailRow.propTypes = {
+    version: PropTypes.string.isRequired,
+    certificate: PropTypes.object.isRequired
+};
